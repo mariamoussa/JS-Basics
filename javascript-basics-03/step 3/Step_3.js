101e6280b7537bc68647f28d3cab168c64c95797
Maria <mariamoussamoussa96@gmail.com>
2021-03-11 16:37:43 +0200

    Step-3

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,14 @@
+function changecolor(x){
+    if(x=="green"){
+        document.getElementById("text").style.color="green";
+
+    }else if(x=="red"){
+        document.getElementById("text").style.color="red";
+
+
+    }else if(x=="blue"){
+        document.getElementById("text").style.color="blue";
+
+
+    }
+}
