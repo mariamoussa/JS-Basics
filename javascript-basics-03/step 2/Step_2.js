70145fa085e84ef9f3dd717cd2f893cf460a50e7
Maria <mariamoussamoussa96@gmail.com>
2021-03-11 16:31:48 +0200

    Step-2

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,10 @@
+function myfunction(x){
+    if (x=="show"){
+    document.getElementById("texte").style.display="block";
+    }else if(x=="hide"){
+
+        document.getElementById("texte").style.display="none";
+
+    }
+
+}
