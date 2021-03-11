b7ce41a75884dc4fb94183c6deffe0be90ea9283
Maria <mariamoussamoussa96@gmail.com>
2021-03-11 14:05:12 +0200

    Step-5

---
+++ javascript-basics-02/step 5/Step_5.js
@@ -0,0 +1,18 @@
+function myFunction(x){
+    if(x== "img1"){
+        document.getElementById("image1").src = "images/image1_2.jpg";
+    }
+    if(x== "img2"){
+        document.getElementById("image2").src = "images/image2_2.jpg";
+    }
+    if(x== "img3"){
+        document.getElementById("image3").src = "images/image3_2.jpg";
+    }
+    if(x== "img4"){
+        document.getElementById("image4").src = "images/image4_2.jpg";
+    }
+    if(x== "img5"){
+        document.getElementById("image5").src = "images/image5_2.jpg";
+    }
+    
+}
