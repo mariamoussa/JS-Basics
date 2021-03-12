cd8b2230193d1748362a7468c6f834e9bc155d6c
Maria <mariamoussamoussa96@gmail.com>
2021-03-12 19:10:12 +0200

    basics-04 done

--- javascript-basics-04-tdd/questions.js
+++ javascript-basics-04-tdd/questions.js
@@ -75,45 +75,54 @@ const BMI = (weight, height) => {
 }
 
 const createLanguagesArray = () => {
-
+    var languageArray=["Html","CSS","Java","PHP"];
+    return languageArray;
 }
 
 const createNumbersArray = () => {
-
+    var numbersArray=[0,1,2,3,4,5];
+    return numbersArray;
 }
 
 const replaceElement = (languages) => {
-
+    languages[2]="Javascript";
+    return languages;
 }
 
 const addElement = (languages) => {
-
+    languages.push("Ruby");
+    languages.push("Python");
+    return languages;
 }
 
 const addNumberElement = (numbers) => {
-
+    numbers.unshift(-1);
+    numbers.unshift(-2);
+    return numbers;
 }
 
 const removeFirst = (languages) => {
-
+    languages.shift();
+    return languages;
 }
 
 const removeLast = (languages) => {
-
+    languages.pop();
+    return languages;
 }
 
 const convertStrToArr = (social_arr) => {
-
+    return social_arr.split(",");
 }
 
 const convertArrToStr = (languages) => {
-
+    return languages.toString();
 }
 
 const sortArr = (social_arr) => {
-
+    return social_arr.sort();
 }
 
 const invertArr = (social_arr) => {
-
+    return social_arr.reverse();
 }
